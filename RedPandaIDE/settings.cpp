65cd1448eeeffbe8365ded3f8c1fd586d9fb743c
李政毅 <142705833+C14147@users.noreply.github.com>
2025-07-29 23:47:40 +0800

    add waiting widget when add compiler

--- RedPandaIDE/settings.cpp
+++ RedPandaIDE/settings.cpp
@@ -3270,6 +3270,9 @@ bool Settings::CompilerSets::addSets(const QString &folder, const QString& c_pro
 bool Settings::CompilerSets::addSets(const QString &folder)
 {
     bool found = false;
+    WaitingWidget waitw;
+    qDebug() << "add sets";
+    waitw.show();
     if (!directoryExists(folder))
         return found;
     if (fileExists(folder, GCC_PROGRAM)) {
@@ -3287,6 +3290,8 @@ bool Settings::CompilerSets::addSets(const QString &folder)
         found=true;
     }
 #endif
+    waitw.hide();
+    waitw.deleteLater();
     return found;
 }
 
--- RedPandaIDE/settingsdialog/extensionswidget.h
+++ RedPandaIDE/settingsdialog/extensionswidget.h
@@ -13,6 +13,7 @@
 #include <QJsonDocument>
 #include <QListWidgetItem>
 
+#include "utils.h"
 #include "settingswidget.h"
 #include "../widgets/macroinfomodel.h"
 #include "../toolsmanager.h"
@@ -34,6 +35,8 @@ public:
     DownloadTool *extFile = nullptr;
     QJsonDocument metadata;
     QMap<QString, QJsonObject> extensionInfoMap; // 存储扩展名到详细信息的映射
+    //UnzipUtil unzip;
+    int unzip_finished = 0;  // 0: no, 1:yes, 2:user abort
 
 public:
     explicit ExtensionsWidget(const QString& name, const QString& group,QWidget *parent = nullptr);
@@ -49,6 +52,8 @@ public slots:
     void dealMetadataDownloadProcess(qint64 bytesRead, qint64 totalBytes, qreal progress);
     void onDownloadExtFinished();
     void dealExtDownloadProcess(qint64 bytesRead, qint64 totalBytes, qreal progress);
+    void dealUnzipProcess(int current, int total);
+    void onUnzipFinished();
 
 private slots:
     void on_extList_itemClicked(QListWidgetItem *item);
