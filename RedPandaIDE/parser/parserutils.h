2e4e08f97d71c4f2cb15beb457e45c38f61e4636
Roy Qu <royqh1979@gmail.com>
2025-10-29 21:10:47 +0800

    - enhancement: Completion suggestion for goto labels;   - fix: Wrong suggestion for complex words.

--- RedPandaIDE/parser/parserutils.h
+++ RedPandaIDE/parser/parserutils.h
@@ -120,6 +120,7 @@ enum class StatementKind  {
     Parameter,
     Block,
     Lambda,
+    Label,
     UserCodeSnippet,  // user code template
     Keyword, // keywords
     KeywordType, //keywords for type (for color management)
