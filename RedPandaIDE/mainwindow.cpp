1ef5b287894042402a547309a26221afd64f137d
Roy Qu <royqh1979@gmail.com>
2025-11-04 21:55:49 +0800

    - fix: Wrongly indent when press tab in the middle of the line.

--- RedPandaIDE/mainwindow.cpp
+++ RedPandaIDE/mainwindow.cpp
@@ -1024,8 +1024,6 @@ void MainWindow::updateCompileActions(const Editor *e)
         disableDebugActions();
     }
     ui->actionStop_Execution->setEnabled(mCompilerManager->running() || mDebugger->executing());
-
-
 }
 
 void MainWindow::updateEditorColorSchemes()
